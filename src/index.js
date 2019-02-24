69146889081dcacb3ca77c8f26f4a2494e503252
fiftik <ripthebest@gmail.com>
2019-02-24 11:35:38 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if (str.length % 2 !== 0) return false;
+
+    var brackets = [];
+
+    [...str].forEach(function (strBracket, i, [...str]) {
+        bracketsConfig.forEach(function (bracketRegex, j, bracketsConfig) {
+            if (bracketRegex[0] !== bracketRegex[1] && bracketRegex[0] === strBracket) {
+                brackets.push(j);
+            } else if (bracketRegex[1] === strBracket && brackets[brackets.length - 1] === j) {
+                brackets.pop();
+            } else if (bracketRegex[0] === bracketRegex[1] && bracketRegex[0] === strBracket) {
+                brackets.push(j);
+            }
+        });
+    });
+
+    if (brackets.length >= 1) {
+        return false;
+    }
+    
+    if (brackets.length == 0) {
+        return true;
+    }
+   
 }
